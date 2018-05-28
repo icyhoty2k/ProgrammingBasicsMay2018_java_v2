31f5b154dc14c2281ced46c10b8239ae6251ca3a
G750JS <ICYHOTY2K@GMAIL.COM>
2018-05-29 01:33:49 +0300

    THIS IS MY FIRST COMMIT TO GIT

---
+++ src/Lesson3LogicalProverki/HW06BonusScore.java
@@ -0,0 +1,35 @@
+package Lesson3LogicalProverki;
+
+import java.util.Scanner;
+
+/**
+ * @author icyhoty2k
+ */
+
+
+public class HW06BonusScore {
+
+    public static void main(String[] args) {
+        Scanner consoleInput = new Scanner(System.in);
+        int points = Integer.parseInt(consoleInput.nextLine());
+        int bonus = 0;
+        if (points <= 100) {
+            bonus = 5;
+        } else if (points > 100 && points < 1000) {
+            points *= 0.2;
+        } else if (points > 1000) {
+            points *= 0.1;
+
+        }
+        if (points % 2 == 0) {
+            bonus += 1;
+        }
+        if (points % 10 == 5) {
+            bonus += 2;
+        }
+        System.out.println(bonus);
+        System.out.println(points + bonus);
+    }
+
+
+}
