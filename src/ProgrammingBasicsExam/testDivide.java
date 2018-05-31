169dcaa89a7fb68a991aa6b283c3f4456dd74ab2
G750JS <ICYHOTY2K@GMAIL.COM>
2018-05-31 23:50:50 +0300

    Help with Zada4a 21 toy shop!

---
+++ src/ProgrammingBasicsExam/testDivide.java
@@ -0,0 +1,13 @@
+package ProgrammingBasicsExam;
+
+/**
+ * @author icyhoty2k
+ */
+
+
+public class testDivide {
+    public static void main(String[] args) {
+        Double d = 16.0 / 3;
+        System.out.println(d);
+    }
+}
