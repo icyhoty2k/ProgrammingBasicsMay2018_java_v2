c0c24e8088dbbd960f1db06244ab17e8c9e4ab18
G750JS <ICYHOTY2K@GMAIL.COM>
2018-06-03 01:14:04 +0300

    New Lesson Complex Conditions HomeWork - part 1

---
+++ src/LessonIVComplexLogicalProverki/HW06PointInRectangleBorder.java
@@ -0,0 +1,36 @@
+package LessonIVComplexLogicalProverki;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * @author icyhoty2k
+ */
+
+
+public class HW06PointInRectangleBorder {
+    public static void main(String[] args) throws IOException {
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
+        double x1, y1, x2, y2, x, y;
+        x1 = Double.parseDouble(scan.readLine());
+        y1 = Double.parseDouble(scan.readLine());
+        x2 = Double.parseDouble(scan.readLine());
+        y2 = Double.parseDouble(scan.readLine());
+        x = Double.parseDouble(scan.readLine());
+        y = Double.parseDouble(scan.readLine());
+        boolean onLeftSide = (x == x1 && y >= y1 && y <= y2);
+        boolean onRightSide = (x == x2 && y >= y1 && y <= y2);
+        boolean onUpSide = (y == y1) && (x >= x1) && (x <= x2);
+        boolean onDownSide = (y == y2) && (x >= x1) && (x <= x2);
+        if (onLeftSide || onRightSide || onUpSide || onDownSide) {
+            System.out.println("Border");
+
+
+        } else {
+            System.out.println("Inside / Outside");
+        }
+
+    }
+}
---
+++ src/LessonIVComplexLogicalProverki/HW5InvalidNumber.java
@@ -0,0 +1,18 @@
+package LessonIVComplexLogicalProverki;
+
+import java.util.Scanner;
+
+/**
+ * @author icyhoty2k
+ */
+
+
+public class HW5InvalidNumber {
+    public static void main(String[] args) {
+        Scanner consoleInput = new Scanner(System.in);
+        int number = Integer.parseInt(consoleInput.nextLine());
+        if (!(number >= 100 && number <= 200 || number == 0)) {
+            System.out.println("invalid");
+        }
+    }
+}
