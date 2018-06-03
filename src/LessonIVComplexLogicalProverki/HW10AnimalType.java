9867cd7708a13e31b85a220288d4d74d37dde80a
G750JS <ICYHOTY2K@GMAIL.COM>
2018-06-03 16:52:18 +0300

    New Lesson Complex Conditions HomeWork - part 1

---
+++ src/LessonIVComplexLogicalProverki/HW10AnimalType.java
@@ -0,0 +1,43 @@
+package LessonIVComplexLogicalProverki;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.System.in;
+
+/**
+ * @author icyhoty2k
+ */
+
+
+public class HW10AnimalType {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String animal = br.readLine().toLowerCase();
+//•	dog -> mammal
+//•	crocodile, tortoise, snake -> reptile
+//•	others -> unknown
+
+        switch (animal) {
+            case "dog": {
+                System.out.println("mammal");
+                break;
+            }
+            case "crocodile": {
+                // System.out.println("reptile");
+            }
+            case "tortoise": {
+                //  System.out.println("reptile");
+            }
+            case "snake": {
+                System.out.println("reptile");
+                break;
+            }
+            default: {
+                System.out.println("unknown");
+            }
+
+        }
+    }
+}
