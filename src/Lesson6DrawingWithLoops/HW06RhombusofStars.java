bcad09d7afaae33346cfa1ff9c458e744e755a01
dell <icyhoty2k@gmail.com>
2018-06-23 05:24:22 +0300

    v5

---
+++ src/Lesson6DrawingWithLoops/HW06RhombusofStars.java
@@ -0,0 +1,30 @@
+package Lesson6DrawingWithLoops;
+
+        import java.util.Scanner;
+
+public class HW06RhombusofStars {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nZvezdi = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < nZvezdi; i++) {
+            for (int j = 0; j < nZvezdi-1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < nZvezdi; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int q = 0; q <nZvezdi-i ; q++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
